273b50f0f8926d3afbc2949a82e0c90cac04b73a
HingeSejal <sejalhinge4@gmail.com>
2026-07-30 10:49:16 +0530

    API creation using Annotations

---
+++ FirstSpring/FirstSpring/src/main/java/com/javaprj/FirstSpring/HelloController.java
@@ -0,0 +1,28 @@
+package com.javaprj.FirstSpring;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return ("Hello, World!");
+//    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name) {
+        return new HelloResponse("Hello, " + name);
+    }
+
+    @GetMapping("/hello")
+    public HelloResponse hello() {
+        return new HelloResponse("Hello, World!");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        return new HelloResponse("Hello, " + name + "!");
+    }
+
+}
